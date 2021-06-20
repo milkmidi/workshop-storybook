1f0bc00d6280d664fb091e996f01a5783be31e4e
milkmidi <milkmidi@gmail.com>
2021-06-20 12:05:36 +0800

    feat: App add TodoListContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-import TodoList from './components/TodoList';
-
-const defaultTodoList = [
-  {
-    id: 'fakeId9',
-    text: 'Workshop storybook',
-    done: false,
-  },
-];
+import TodoListContainer from './containers/TodoListContainer';
 
 export default function App():JSX.Element {
   return (
     <div className="App">
       <h1>hi, React</h1>
-      <TodoList todoData={defaultTodoList} />
+      <TodoListContainer />
     </div>
   );
 }
